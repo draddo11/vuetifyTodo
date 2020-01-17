bf934452c8302379fe2d8e757907cfd36cb364b7
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-17 22:18:46 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id ="app">
+       <h3>{{msg}}</h3>
+       <h6> {{title}}</h6>
+       <div class="col-md-12"> 
+    <Character 
+    v-for="(id,index) in initial_ids"
+    :id="id"
+    key= "index"
+    />
+    
+     </div>
+    </div>
+</template>
+
+<script>
+ import Character from './components/Character.vue'
+
+export default {
+    name: 'app',
+    data(){
+        return {
+            title: 'Generate Your team here now ',
+            msg:'WELCOME TO THE GALAXY FAR FAR AWAY   ',
+            initial_ids:[1,13,14]
+        }
+    },
+
+  components: {
+    Character
+    }
+}
+</script>
