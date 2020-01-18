c5e66e620f0edc430db2004c1a6e5a5311661f89
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-18 21:21:02 +0000

    aded vuex with vue add

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,20 @@
   <v-app >
     
     <v-content>
-      </>
+      <router-view></router-view>
     </v-content>
+    
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+  
   },
 
   data: () => ({
