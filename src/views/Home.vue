4b970f478c0c3e0e2f730aab07fc49132a0b66b9
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-18 22:54:37 +0000

    added text and color mods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 i<template>
   <div class="home">
-    <h1>This is a home page</h1>
+      <h1>HomePage</h1>
+    <h4>This is a home page</h4>
+    
+    <p class='red white--text'> Lorem ipsum dolor sit amet consectetur adipisicing elit. At exercitationem tenetur distinctio, repellendus quia dolor, minima possimus, cumque consequatur repellat voluptate veniam soluta. Modi illum dolorem expedita neque fuga consequuntur assumenda quaerat sit. Corrupti delectus ea rem quo explicabo dolorum veniam, tempora dolores quae aspernatur, totam soluta praesentium nemo ipsa voluptatum sapiente neque. Fugiat explicabo blanditiis, vitae tempore, doloribus magnam deserunt rerum voluptatem saepe, reprehenderit adipisci incidunt nemo ab aspernatur! Aliquid veritatis amet, quidem fugiat cumque sed dicta nulla, et incidunt accusantium ut inventore repudiandae. Similique sapiente doloribus accusamus aperiam delectus accusantium qui, explicabo porro nisi rerum voluptatum vitae veritatis?</p>
+
+    <p class= "pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, doloremque quam. Cupiditate necessitatibus tempore labore quasi excepturi reiciendis odio rerum accusantium in sit autem, ex nam soluta consequatur eaque, laborum, facere pariatur quam! Debitis, magnam neque voluptatum adipisci reiciendis dolor quisquam optio illo aut fuga qui animi deserunt voluptate eius corrupti veritatis vero molestiae ab reprehenderit nam fugit repudiandae non consequatur rem. Fugit, alias. Culpa dignissimos voluptas ab, tenetur ipsum quaerat deleniti aut hic ut aliquid atque magni nulla veniam unde eligendi sed recusandae accusantium provident fuga quod corrupti esse dolores doloremque repellendus? Porro necessitatibus ea, autem accusantium ex quo.</p>
   </div>
 </template>
