a89df210e84c391c0e6525ca2d8cb87bae9ba5e1
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-20 19:29:32 +0000

    added more button types

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
       <h1>HomePage</h1>
 
    <v-btn class="pink white--text"> Normal btn </v-btn>
-   <hr>
+   
    <v-btn color="yellow" depressed> depressed </v-btn>
    <hr>
-   <v-btn flat color="blue" >flat btn</v-btn>
+   <v-btn rounded flat color="blue" >flat btn</v-btn>
     <hr>
     <v-btn  depressed  class="pink white--text" > 
-        <v-icon left >email</v-icon>
+        <v-icon left >fa-email</v-icon>
         <span>email me </span>
 
     </v-btn>
