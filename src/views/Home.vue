7b3e20bdcb9d258771f4e8de913690103efef70f
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-20 12:45:35 +0000

    worked on the button in the home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="home">
       <h1>HomePage</h1>
-   <v-btn class="pink white-text">Click me </v-btn>
+
+   <v-btn class="pink white--text"> Normal btn </v-btn>
    <hr>
-   <v-btn color="pink white-text">Click me </v-btn>
+   <v-btn color="yellow" depressed> depressed </v-btn>
+   <hr>
+   <v-btn flat color="blue" >flat btn</v-btn>
+    <hr>
+    <v-btn  depressed  class="pink white--text" > 
+        <v-icon left >email</v-icon>
+        <span>email me </span>
+
+    </v-btn>
+
   </div>
 </template>
+
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  
