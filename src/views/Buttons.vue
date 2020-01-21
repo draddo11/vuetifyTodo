70df00c566d1a9846b36dc490c8730f3540c01a2
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-21 21:56:54 +0000

    started breaking piont

---
+++ src/views/Buttons.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+      <h1>HomePage</h1>
+
+   <v-btn class="pink white--text"> Normal btn </v-btn>
+   
+   <v-btn color="yellow" depressed> depressed </v-btn>
+   
+   <v-btn rounded flat color="blue" >flat btn</v-btn>
+   
+
+    <v-btn  depressed  class="pink white--text" > 
+        <v-icon left> email</v-icon>
+        <span>email me </span>
+    </v-btn>
+  
+    <v-btn class="ma-2" dark>
+        <v-icon dark left>mdi-minus_circle</v-icon>Cancel
+      </v-btn>
+
+  <v-btn  depressed small   class="pink white--text" > 
+        <v-icon left> email</v-icon>
+        <span>email me </span>
+    </v-btn>
+
+
+     <v-btn  depressed large class="pink white--text" > 
+        <span>email me </span>
+         <v-icon right > email</v-icon>
+    </v-btn>
+
+    <v-btn fab  depressed small dark  color="purple" > 
+         <v-icon  > favourite </v-icon>
+    </v-btn>
+
+
+
+
+  </div>
+</template>
+
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="home">
       <h1>HomePage</h1>
-
-   <v-btn class="pink white--text"> Normal btn </v-btn>
-   
-   <v-btn color="yellow" depressed> depressed </v-btn>
-   
-   <v-btn rounded flat color="blue" >flat btn</v-btn>
-   
-
-    <v-btn  depressed  class="pink white--text" > 
-        <v-icon left> email</v-icon>
-        <span>email me </span>
-    </v-btn>
-  
-    <v-btn class="ma-2" dark>
-        <v-icon dark left>mdi-minus_circle</v-icon>Cancel
-      </v-btn>
-  <v-btn  depressed  class="pink white--text" > 
-        <v-icon left> email</v-icon>
-        <span>email me </span>
-    </v-btn>
+      <!-- uses break pionts  -->
+<v-btn class="hidden-md-and-down" color= 'purple'> Click Me </v-btn>
 
 
   </div>
