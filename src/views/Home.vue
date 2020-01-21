8e51eb3bd476d1c5a19943f0ba1c88e85fe17bf7
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-21 20:54:44 +0000

    font icon working now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,22 @@
    <v-btn class="pink white--text"> Normal btn </v-btn>
    
    <v-btn color="yellow" depressed> depressed </v-btn>
-   <hr>
+   
    <v-btn rounded flat color="blue" >flat btn</v-btn>
-    <hr>
-    <v-btn  depressed  class="pink white--text" > 
-        <v-icon left >fa-email</v-icon>
-        <span>email me </span>
+   
 
+    <v-btn  depressed  class="pink white--text" > 
+        <v-icon left> email</v-icon>
+        <span>email me </span>
     </v-btn>
-    <div>
+  
     <v-btn class="ma-2" dark>
         <v-icon dark left>mdi-minus_circle</v-icon>Cancel
       </v-btn>
-    </div>
+  <v-btn  depressed  class="pink white--text" > 
+        <v-icon left> email</v-icon>
+        <span>email me </span>
+    </v-btn>
 
 
   </div>
