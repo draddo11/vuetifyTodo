8daa22a2af7d4a60c3dab21c6db7f902839e418a
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-24 16:49:26 +0000

    added sign in button

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
     <v-content>
       <router-view></router-view>
     </v-content>
-    
   </v-app>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar';
+
 
 export default {
   
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,24 @@
 <template>
-<nav >
+<nav>
     <v-toolbar flat app >
         <v-toolbar-title>
             <span class="font-weight-light"> Todo</span>
             <span> Vuetify</span>
         </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn  flat  color= "grey">
+            <span>Sign Out</span>
+             <v-icon right>
+                 exit_to_app 
+          </v-icon>
+        </v-btn>
     </v-toolbar>
 </nav>
 
 </template>
 
     <script>
+    
     export default {
 
     }
