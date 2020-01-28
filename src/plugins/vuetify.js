dc2d711b92bb58b0f4b92ff9119594a237e812a8
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-28 16:20:23 +0000

    changed the theme colors in plugins

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,7 +6,13 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     icons: {
-        iconfont: 'md',  
+        iconfont: 'md', 
+        theme:{
+          primary: '#9652ff',
+          success: '#3cd1c2',
+          info:'#ffaa2c',
+          error:'#f83e70'
+        } 
         
       },
      
