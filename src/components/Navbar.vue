84403d6dc87cd755c334189488ab6190e3050d45
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-01 22:06:21 +0000

    navigation drawer working

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
         <v-app-bar-nav-icon class= "grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title class= "text-uppercase grey--text">
             <span class="font-weight-light"> Todo</span>
-            <span> Vuetify</span>
+                Vuetify
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn  flat  color= "grey">
@@ -18,15 +18,15 @@
     <v-navigation-drawer app v-model="drawer"  class= "primary">
        <v-list>
            <v-list-tile >
-                <v-divider></v-divider>
-             <v-list-title-content v-for="link in links" :key="link.text"> 
-                 <v-list-tile-action>
+              
+             <v-list-title-content  v-for="link in links" :key="link.text"> 
+                 <v-list-tile-action >
                      <v-list-item-icon>
                      <v-icon class="white--text">{{link.icon}}</v-icon>
                      </v-list-item-icon>
                 </v-list-tile-action>
-                 <v-divider></v-divider>
-                <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+                 
+                <v-list-tile-title name=" " class="white--text">{{link.text}}</v-list-tile-title>
                  </v-list-title-content> 
             </v-list-tile> 
        </v-list>
