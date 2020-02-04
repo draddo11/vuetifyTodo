ae8c7e57fd3deb7a0a609ea31c0cb827260a913a
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-04 23:07:17 +0000

    fixed double dashboard issue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app class="grey lighten-4">
     <Dashboard/>
     <v-content>
-      <router-view></router-view>
+      <!-- <router-view></router-view> -->
     </v-content>
   </v-app>
 </template>
