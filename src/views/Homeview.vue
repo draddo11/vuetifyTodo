ee958235e67bcae0326a6912eb89b20d104e8612
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-18 23:22:35 +0000

    bug fixes

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -4,11 +4,10 @@
     <h3>Hire expert Vue.js developers & find Vue.js jobs
 Sign up and post a job or create your developer profile.
 We will find offers / candidates for you!</h3>
-   
+  
    <Footer/>
   </div>
 
- 
 </template>
 
 <script>
