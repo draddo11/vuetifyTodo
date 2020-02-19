2c32be507b5148a1f309008381a2d2db40c02fca
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-19 22:57:39 +0000

    changing ui

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -5,11 +5,11 @@
 Sign up and post a job or create your developer profile.
 We will find offers / candidates for you!</h3>
 <v-card
-    max-width="auto"
     class="mx-auto"
+    max-width="500"
   >
-    <!-- <v-system-bar
-      color="pink darken-2"
+    <v-system-bar
+      color="indigo darken-2"
       dark
     >
       <v-spacer></v-spacer>
@@ -19,23 +19,60 @@ We will find offers / candidates for you!</h3>
       <v-icon>mdi-window-maximize</v-icon>
 
       <v-icon>mdi-close</v-icon>
-    </v-system-bar> -->
+    </v-system-bar>
 
-    <!-- <v-app-bar
+    <v-toolbar
+      color="indigo"
       dark
-      color="pink"
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>My Music</v-toolbar-title>
+      <v-toolbar-title>Discover</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-    </v-app-bar> -->
+    </v-toolbar>
 
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
     <v-container>
       <v-row dense>
         <v-col cols="12">
@@ -84,7 +121,7 @@ We will find offers / candidates for you!</h3>
         </v-col>
       </v-row>
     </v-container>
-  </v-card>
+  <!-- </v-card>  -->
   
    <Footer/>
   </div>
