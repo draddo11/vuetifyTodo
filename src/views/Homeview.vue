d1c4d5f2b7a9cca1dfda249417ba7e7f18f9c59b
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-19 23:28:26 +0000

    added more components

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -135,5 +135,11 @@ We will find offers / candidates for you!</h3>
     components:{
       Footer,
     }, 
+     cards: [
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+    ],
   }
+
 </script>
