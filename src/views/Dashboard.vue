93927d7dbdb3358b38eaf0390c7ebdf81d3d8b05
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-20 23:56:05 +0000

    edited to add btn for job times

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,8 +5,10 @@
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
     Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
     <template v-slot:actions>
-      <v-btn text color="primary">Dismiss</v-btn>
-      <v-btn text color="primary">Retry</v-btn>
+      <v-btn text color="primary">Remote</v-btn>
+      <v-btn text color="primary">Full time</v-btn>
+      <v-btn text color="primary">Internships</v-btn>
+      <v-btn text color="primary">Part time</v-btn>
     </template>
   </v-banner>
       <div id="app">
