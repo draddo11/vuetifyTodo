9f7b179f7de8ad6d0bcbd919adcc5c3df3a9f4d1
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-20 23:50:47 +0000

    added a banner

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,14 @@
 <template>
   <div class= "dashboard">
       <h1>Dashboard </h1>
+      <v-banner>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+    Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+    <template v-slot:actions>
+      <v-btn text color="primary">Dismiss</v-btn>
+      <v-btn text color="primary">Retry</v-btn>
+    </template>
+  </v-banner>
       <div id="app">
   <v-app id="inspire">
     <v-card
