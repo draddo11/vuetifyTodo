7f185c2dda150713ff5629f190ad6c9411fb8b1f
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-27 23:34:27 +0000

    added the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -30,7 +30,7 @@
        <v-list>
            <v-list>
               
-             <v-list-tile  v-for="link in links" :key="link.text" router :to="link.route"> 
+             <v-list-tile name="i" v-for="link in links" :key="link.text" router :to="link.route"> 
                  <v-list-item-action >
                      <v-list-item>
                      <v-list-item-icon>
