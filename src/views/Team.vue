8afdc3369704ea4b6a37fd955326199726a30aeb
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-02-29 22:48:36 +0000

    team component js component

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -39,8 +39,17 @@
 
 <script>
   export default {
-     
+     data () {
+      return {
+        tab: null,
+        items: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
   }
+  
 </script>
 
 <style>
