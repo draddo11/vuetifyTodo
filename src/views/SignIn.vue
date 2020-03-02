f442f620d13390d644c877c74eefc05609c76357
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-03-02 13:55:52 +0000

    working on google logo 2 in sign up component

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -28,7 +28,7 @@
        <v-img 
        width="10px"
        height="10px"
-       src="http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F10%2Fnexus2cee_Search-Thumb-150x150.png&w=150&h=150&zc=3">  Google
+       src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"  Google
        </v-img>
         <v-icon color= "white " right >
         mdi-email
