9d2adcbe1e859282e73ecc11f8af300b3fe46a86
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-03-03 23:52:10 +0000

    added google search input  2 in dashboard  component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -78,18 +78,7 @@
         </v-list-item-action>
       </template>
     </v-autocomplete>
-    <template v-slot:extension>
-      <v-tabs
-        v-model="tab"
-        :hide-slider="!model"
-        color="blue-grey"
-        slider-color="blue-grey"
-      >
-        <v-tab :disabled="!model">News</v-tab>
-        <v-tab :disabled="!model">Trading</v-tab>
-        <v-tab :disabled="!model">Blog</v-tab>
-      </v-tabs>
-    </template>
+    
   </v-toolbar>
 </template>
 
