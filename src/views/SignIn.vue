aa13789c822afb9f3070cedf213d27ce704b361a
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-03-03 22:45:55 +0000

    added google login button to sign in component with width

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -166,7 +166,7 @@ $google-blue: #4285f4;
 $button-active-blue: #1669F2;
 
 .google-btn {
-  width: 184px;
+  width: 304px;
   height: 42px;
   background-color: $google-blue;
   border-radius: 2px;
