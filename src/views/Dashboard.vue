c56a60edb5ab16dd3a4be4943ec547c67c2303fc
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-03-04 00:12:10 +0000

    removed unused  cards  in dashboard   component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -24,6 +24,7 @@
 
     
   </v-banner>
+  
 <template>
   <v-toolbar color="orange accent-1">
     <v-app-bar-nav-icon class="hidden-sm-and-down"></v-app-bar-nav-icon>
@@ -124,68 +125,11 @@
     </v-card>
   </v-app>
   
+</div>
 
-  <div id="app">
-  <v-app id="inspire">
-    <v-card
-      max-width="400"
-      class="mx-auto"
-    >
-         <v-row dense>
-          <v-col cols="12"
-          md=12>
-            <v-card
-              color="#385F73"
-              dark
-            >
-              <v-card-title class="headline">Unlimited music now</v-card-title>
-  
-              <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
-  
-              <v-card-actions>
-                <v-btn text>Listen Now</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-  
-          <v-col
-            v-for="(item, i) in items"
-            :key="i"
-            cols="12"
-          >
-            <v-card
-              :color="item.color"
-              dark
-            >
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                  <v-card-title
-                    class="headline"
-                    v-text="item.title"
-                  ></v-card-title>
-  
-                  <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-                </div>
-  
-                <v-avatar
-                  class="ma-3"
-                  size="125"
-                  tile
-                >
-                  <v-img :src="item.src"></v-img>
-                </v-avatar>
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
-      
-    </v-card>
-  </v-app>
 </div>
-</div>
-  </div>
-  
-  
+
+
   
 </template>
 
