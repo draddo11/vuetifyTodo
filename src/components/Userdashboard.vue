838db8abddc2916db8b76f5e444f10f729ceb5f6
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-03-27 10:09:58 +0000

    added icon to the userdasboard component

---
+++ src/components/Userdashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div elevation=20>
+        <v-card max-width="320"
+     height="110"
+    class="mx-auto">
+            <v-icon>person_outline</v-icon>
+
+
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
