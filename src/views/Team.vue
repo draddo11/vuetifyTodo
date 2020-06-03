cad1ab9a897b09723902e2b97bab94e4e5f924f9
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-06-03 23:55:04 +0000

    addin to team component

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,55 +1,21 @@
 <template>
-  <v-card color="basil">
-    <v-card-title class="text-center justify-center py-6">
-      <h1 class="font-weight-bold display-3 basil--text">BASiL</h1>
-    </v-card-title>
+<a :href="email" target="_blank" rel="noopener noreferrer">
+  <v-icon>"email"</v-icon>
+</a>
+<a :href="people" target="_blank" rel="noopener noreferrer">
+<v-icon>"people"</v-icon>
+</a>
 
-    <v-tabs
-      v-model="tab"
-      background-color="transparent"
-      color="basil"
-      grow
-    >
-      <v-tab
-        v-for="item in items"
-        :key="item"
-      >
-        {{ item }}
-      </v-tab>
-    </v-tabs>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-        v-for="item in items"
-        :key="item"
-      >
-        <v-card
-          color="basil"
-          flat
-        >
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
 </template>
-
-
-
-
-<script>
-  export default {
-     data () {
-      return {
-        tab: null,
-        items: [
-          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
-        ],
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  <script>
+  export default{
+    data(){
+      return{
+        email:"https://www.google.com",
+        people:"https://www.facebook.com"
       }
-    },
+    }
   }
-  
 </script>
 
 <style>
